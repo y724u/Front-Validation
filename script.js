d8ad17b9d9204d8f2f56a404935231f476070b01
y724u <68385107+y724u@users.noreply.github.com>
2020-11-27 21:08:20 +0900

    disabled をテスト

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
   // フロントバリデーション
   // - 入力の必須チェックを行う
 
@@ -10,17 +10,31 @@ $(function() {
 
 
 
-// 必須項目のテキストフォーカスが外れたときに、そこに値が入っているかチェックする
-$('.js-textCheck').on('blur',function() {
-  if($(this).val() === '')
-  // もし値が入ってない場合は、classを付け替える
-  $('.validation__text').attr('class','validation__alert');
-  alert('名前を入力してください！');
+  // 必須項目のテキストフォーカスが外れたときに、そこに値が入っているかチェックする
+  $('.js-textCheck').on('blur', function () {
+    if ($(this).val() === '') {
+      // もし値が入ってない場合は、classを付け替える
+      $($(this)).attr('class', 'validation__alert');
+      $($(this)).next().addClass('show__notice');
+      // alert('名前を入力してください！');
+    }
+    else {
+      $($(this)).attr('class', 'validation__text');
+      // 入っている場合はそのままにする
+      $($(this)).next().removeClass('show__notice');
+    }
 
-  // 入っている場合はそのままにする
   });
 
-
+  // 必須項目のテキストフォーカスが外れたときに、2つのjs-textCheckに値が入っているかチェックする
+  $('.test , .test1').on('change', function () {
+    if ($('.test1').val() !== "" && $('.test').val() !== false) {
+      // $('.validation__registration').addClass('button__allow');
+      $('#registration').prop('disabled', false);
+    } else {
+      $('#registration').prop('disabled', true);
+    }
+  });
 
   // $('.js-checkbox').on('change', function () {
   //   if ($(this).prop('checked'))
@@ -31,18 +45,13 @@ $('.js-textCheck').on('blur',function() {
 
 
 
-// 値が入力されていなければaddClsass「入力必須項目です」とフォームの下に表示する
+  // 値が入力されていなければaddClsas「入力必須項目です」とフォームの下に表示する
 
-// 値が入力されていれば何もしない
-
-
-
-// ２つの、必須項目にテキストが入っていればaddクラス、pointer-eventを付与する
-
-// １つでも入っていない場合は、removeClass
+  // 値が入力されていれば何もしない
 
 
 
+  // ２つの、必須項目にテキストが入っていればaddクラス、pointer-eventを付与する
 
-
+  // １つでも入っていない場合は、removeClass
 });
