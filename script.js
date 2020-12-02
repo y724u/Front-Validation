97449478c483eb666d72bbc98d51e1fd8a2cb26a
y724u <68385107+y724u@users.noreply.github.com>
2020-12-02 14:21:36 +0900

    インデントを修正

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 $(function () {
-  // // 必須項目のテキストフォーカスが外れたときに、そこに値が入っているかチェックする
-  // $('.js-textCheck').on('blur', function () {
-  //   const $this = $(this);
-  //   const $noticeClass = $this.next('.js-notice');
-  //   // もし値が入ってない場合は、alertClassを付ける
-  //   if ($this.val() === '') {
-  //     $($this).attr('class', 'validation__alert');
-  //     // inputの下にnoticeを表示する
-  //     $($noticeClass).addClass('show__notice');
-  //   }
-  //   else {
-  //     // 値が入っている場合はtextClassをつける
-  //     $($this).attr('class', 'validation__text');
-  //     // inputの下にnoticeを表示しない
-  //     $($noticeClass).removeClass('show__notice');
-  //   }
-  // });
-
-  // 必須項目のテキストがチェンジした時に、そこに値が入っているかチェックする
+  // 必須項目に値が入ってない場合スタイルとエラーの表示をする
   $('.js-textCheck').on('change', function () {
     const $this = $(this);
     // もし値が入ってない場合は、is-errorを付ける
@@ -34,8 +16,9 @@ $(function () {
     }
   });
 
-  //input:requiredがチェンジした時
+  // 必須項目に値が入ってたらボタンを活性化させる
   const $target = '.js-list input:required';
+  //input:requiredがチェンジした時
   $($target).on('change', function () {
     let flag = true;
     // どちらかの値が空白の場合＝false
@@ -45,36 +28,11 @@ $(function () {
       }
     });
     if (flag) {
-      // もし必須項目のテキストがどちらも空白じゃない場合
+      // もし必須項目のテキストが全て空白じゃない場合
       $('#js-registration').prop('disabled', false);
     } else {
       // もし必須項目のテキストに空白がある場合
       $('#js-registration').prop('disabled', true);
     }
-
-    // if (
-    //   $('input:required').val() !== ''
-    //   // $('#js-inputText2').val() !== ''
-    // ) {
-    //   // ボタン活性化のクラスをつける
-    //   $('#js-registration').prop('disabled', false);
-    // } else {
-    //   // 空白がある場合はクラスを外す
-    //   $('#js-registration').prop('disabled', true);
-    // }
   });
-  // // テキストのフォーカスが外れた時
-  // $('.js-list').on('change blur', function () {
-  //   // もし必須項目のテキストがどちらも空白じゃない場合
-  //   if (
-  //     $('input:required').val() !== ''
-  //     // $('#js-inputText2').val() !== ''
-  //   ) {
-  //     // ボタン活性化のクラスをつける
-  //     $('#js-registration').prop('disabled', false);
-  //   } else {
-  //     // 空白がある場合はクラスを外す
-  //     $('#js-registration').prop('disabled', true);
-  //   }
-  // });
 });
